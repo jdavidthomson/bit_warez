925b508067d7df56dabd699bb590f53ef8441599
David Thomson <jdavidthomson@hotmail.com>
2018-05-24 20:20:41 -0400

    initial POC of platform - less BTCD integration

---
+++ handlers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "net/http"
+    "log"
+    "encoding/json"
+    "io/ioutil"
+)
+type ProcessJson struct{
+	Process		map[string]interface{}		`json:"process"`
+}
+
+func SubmitProcess(w http.ResponseWriter, r *http.Request){
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	var process_json ProcessJson
+	err = json.Unmarshal(body, &process_json)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	log.Println(process_json.Process["process_name"].(string))
+	arguments := process_json.Process["process_arguments"].([]interface{})
+	for _, value := range arguments{
+		log.Println(value.(map[string]interface{}))
+	}
+}
+func PaidEndpoint(w http.ResponseWriter, r *http.Request){
+
+}
